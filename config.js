9112304b0b1ff32eedcd995efd907ce889160ef8
Mathieu Decoene <hello@mathieudecoene.be>
2013-09-06 15:17:18 +0100

    Added routes

--- config.js
+++ config.js
@@ -1,5 +1,17 @@
 var config = module.exports = {};
 
+config.app = {
+    'title': 'LibrarySearch'
+}
+
 config.server = {
     'port': 1234
 };
+
+config.auth = {
+    'summon': {
+        'id':   'cam',
+        'key':  'n74nDv98EFz8hE1wVNoq3MgrU66a9r4M'
+    },
+    'aquabrowser': {}
+}
