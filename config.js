969aad20d57ecd627cc2efbc15db4ad773ee58ae
Mathieu Decoene <hello@mathieudecoene.be>
2013-09-10 08:38:23 +0100

    Updated config file

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ config.constants = {
     'engines': {
         'summon': {
             'auth': {
-                'id':   'cam',
-                'key':  'n74nDv98EFz8hE1wVNoq3MgrU66a9r4M'
+                'id':   '[ID]',
+                'key':  '[KEY]'
             },
             'uri': 'api.summon.serialssolutions.com',
             'version': '/2.0.0/search'
